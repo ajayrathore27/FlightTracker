0cc8a48a22e61b80c2b1576e8dc060ce16491a8e
Ajay <ajay.rathore27@gmail.com>
2026-03-19 16:18:17 -0500

    First commit

---
+++ app.py
@@ -0,0 +1,6 @@
+print("Welcome to Flight Tracker")
+
+def search_flights(source, destination):
+    print(f"Searching flights from {source} to {destination}...")
+
+search_flights("Dallas", "New York")
---
+++ main.py
@@ -0,0 +1,1 @@
+print("Welcome to FLight Tracker")
